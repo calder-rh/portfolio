ceb072971b097bf0bb3f877ba3244d152fe29a3e
CalderH <caldertheperson@me.com>
2024-06-10 14:33:11 -0400

    squarified first oval

--- src/scripts/clip-paths.js
+++ src/scripts/clip-paths.js
@@ -4,24 +4,24 @@ function center() {
   return random(47, 53);
 }
 
-function handle() {
-  return random(25, 35)
+function handle(squareness) {
+  return random(squareness - 3, squareness + 3);
 }
 
-function randomAnchor() {
+function randomAnchor(squareness) {
   const middleValue = center();
   return {
-    low: middleValue - handle(),
+    low: middleValue - handle(squareness),
     middle: middleValue,
-    high: middleValue + handle()
+    high: middleValue + handle(squareness)
   }
 }
 
-export default function randomOvalPath() {
-  const { low: tl, middle: t, high: tr } = randomAnchor();
-  const { low: rt, middle: r, high: rb } = randomAnchor();
-  const { low: bl, middle: b, high: br } = randomAnchor();
-  const { low: lt, middle: l, high: lb } = randomAnchor();
+export default function randomOvalPath(squareness) {
+  const { low: tl, middle: t, high: tr } = randomAnchor(squareness);
+  const { low: rt, middle: r, high: rb } = randomAnchor(squareness);
+  const { low: bl, middle: b, high: br } = randomAnchor(squareness);
+  const { low: lt, middle: l, high: lb } = randomAnchor(squareness);
   const pathStr = `M ${t} 0 C ${tr} 0, 100 ${rt}, 100 ${r} C 100 ${rb}, ${br} 100, ${b} 100 C ${bl} 100, 0 ${lb}, 0 ${l} C 0 ${lt}, ${tl} 0, ${t} 0`;
   return pathStr;
 }
---
+++ src/scripts/squish-ovals.js
@@ -0,0 +1,25 @@
+import random from "./random";
+
+function squishOvals() {
+  window.requestAnimationFrame(() => {
+    document.querySelectorAll('.oval.squish').forEach((element) => {
+      const position = element.getBoundingClientRect().top / document.documentElement.clientHeight;
+      const mobile = document.documentElement.clientWidth < 500;
+      const minHeight = 50;
+      const maxHeight = mobile ? 300 : 500;
+      if (position < 0) {
+        element.style.height = `${maxHeight}px`;
+      } else if (position > 1) {
+        element.style.height = `${minHeight}px`;
+      } else {
+        const a = (1 - position);
+        const newHeight = maxHeight * a + minHeight * (1 - a);
+        element.style.height = `${newHeight}px`;
+      }
+    })
+  });
+}
+
+document.addEventListener("DOMContentLoaded", squishOvals);
+document.addEventListener("scroll", squishOvals);
+window.addEventListener("resize", squishOvals);
