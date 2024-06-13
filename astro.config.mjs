947d6c9652b028ed45205c8f0042c01cc3aa4932
CalderH <caldertheperson@me.com>
2024-06-13 12:45:18 -0400

    change location of d16 page to links dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://calder-rh.github.io',
+  redirects: {
+    '/work/d16': '/links/d16-workshop'
+  }
 });
