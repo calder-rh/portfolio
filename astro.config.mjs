8f8cf5d723454f95bdf90c807e7fe279e67dd6cc
CalderH <caldertheperson@me.com>
2024-06-13 14:08:04 -0400

    fix redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://calder-rh.github.io',
+  site: 'https://calder-rh.github.io/portfolio',
   redirects: {
     '/work/d16': '/links/d16-workshop'
   }
