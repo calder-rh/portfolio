3509cf048164208d193df7f0e0fd2239d0055c82
CalderH <caldertheperson@me.com>
2024-06-21 15:00:13 -0400

    testing column layout

--- src/scripts/squish-ovals.js
+++ src/scripts/squish-ovals.js
@@ -1,18 +1,85 @@
-const squishes = document.querySelectorAll('.squish');
+import $ from 'jquery';
+import random from "./random";
+
+const columnWidth = 400;
+
+const squishList = $('.squish');
+
+function setupSquishes() {
+  for (let i = 0; i < squishList.length; i++) {
+    let squish = $(squishList[i]);
+    squish.data('order', i);
+    const maxHeight = random(300, 500);
+    squish.data('max-height', maxHeight);
+    squish.data('power', 1);
+    squish.css('height', `${maxHeight}px`);
+  }
+}
+
+function putOvalsInWaitingRoom() {
+  const squishList = $('.squish');
+  squishList.appendTo('#oval-waiting-room');
+}
+
+let prevNumColumns = 0;
+
+function parabolicPadding(i, numColumns) {
+  const maxPadding = 50;
+  const a = i / (numColumns - 1);
+  return (1 + 4 * a * (a - 1)) * maxPadding;
+}
+
+function setupColumns() {
+  window.requestAnimationFrame(() => {
+    const bodyWidth = $('body').width();
+    let numColumns = Math.floor(bodyWidth / columnWidth);
+    if (numColumns == 0) numColumns = 1;
+
+    if (numColumns !== prevNumColumns) {
+      prevNumColumns = numColumns;
+      const columnsContainer = $('#oval-columns');
+    
+      putOvalsInWaitingRoom();
+      columnsContainer.empty()
+
+      const columnsArray = [];
+      const paddingAdjustment = (numColumns % 2 == 0) ? parabolicPadding(numColumns / 2, numColumns) : 0;
+
+      const heights = [];
+      for (let i = 0; i < numColumns; i++) {
+        let d = $('<div>', {id: `col${i}`, class: 'oval-column'});
+        const padding = parabolicPadding(i, numColumns) - paddingAdjustment;
+        heights.push(padding);
+        d.css('padding-top', padding);
+        d.appendTo(columnsContainer);
+        columnsArray.push(d);
+      }
+
+      squishList.each((index, squish) => {
+        const minIndex = heights.reduce((minIdx, currentValue, currentIndex, arr) => {
+          return currentValue < arr[minIdx] ? currentIndex : minIdx;
+        }, 0);
+        $(columnsArray[minIndex]).append(squish);
+        heights[minIndex] += $(squish).data('max-height');
+      })
+    }
+  });
+}
+
 
 function squishOvals() {
   window.requestAnimationFrame(() => {
-    squishes.forEach((element) => {
+    squishList.each((idx, element) => {
       const position = element.getBoundingClientRect().top / document.documentElement.clientHeight;
       const mobile = document.documentElement.clientWidth < 500;
       const minHeight = 50;
-      const maxHeight = mobile ? 300 : 500;
+      const maxHeight = $(element).data('max-height') * (mobile? 0.7 : 1);
       if (position < 0) {
         element.style.height = `${maxHeight}px`;
       } else if (position > 1) {
         element.style.height = `${minHeight}px`;
       } else {
-        const a = (1 - position);
+        const a = (1 - position) ** $(element).data('power');
         const newHeight = maxHeight * a + minHeight * (1 - a);
         element.style.height = `${newHeight}px`;
       }
@@ -20,6 +87,20 @@ function squishOvals() {
   });
 }
 
-document.addEventListener("DOMContentLoaded", squishOvals);
-document.addEventListener("mousewheel", squishOvals);
-window.addEventListener("resize", squishOvals);
+// document.addEventListener("DOMContentLoaded", squishOvals);
+// document.addEventListener("mousewheel", squishOvals);
+
+function ready() {
+  setupSquishes();
+  setupColumns();
+  squishOvals();
+}
+
+function resize() {
+  setupColumns();
+  squishOvals();
+}
+
+$(document).ready(ready);
+$(window).on('scroll', squishOvals);
+$(window).on('resize', resize);
