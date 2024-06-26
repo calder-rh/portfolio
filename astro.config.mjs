be2a05d7597574cb8a73a058ee7cbca74afc05d7
CalderH <caldertheperson@me.com>
2024-06-26 16:07:54 -0400

    starting to figure out how to auto populate homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://calder-rh.github.io/portfolio',
   redirects: {
     '/work/d16': '../links/d16-workshop'
-  }
+  },
+  integrations: [mdx()]
 });
