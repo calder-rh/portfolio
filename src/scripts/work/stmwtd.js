65c700185652a09e1792ed983474f3903e15650a
CalderH <caldertheperson@me.com>
2026-03-13 15:19:14 -0400

    better stmwtd animation

--- src/scripts/work/stmwtd.js
+++ src/scripts/work/stmwtd.js
@@ -11,33 +11,49 @@ const slider = document.getElementById("slider")
 const tl = gsap.timeline({ paused: true })
 
 const opacities = [
-  {words: 0, letters: 0},
-  {words: 1, letters: 0},
-  {words: 1, letters: 1},
+  {words: 0, connectors: 0, letters: 0},
+  {words: 1, connectors: 0, letters: 0},
+  {words: 1, connectors: 0, letters: 0},
+  {words: 1, connectors: 1, letters: 0},
+  {words: 0, connectors: 1, letters: 1},
 ]
 
+const durations = [
+  1, 1, 0.5, 1
+]
 
+function animIDs(el, ancestors=[]) {
+  if (ancestors.length !== 0) {
+    const idStr = ancestors.join('-')
+    console.log(idStr)
+    el.dataset.animId = idStr
+  }
+  let unnamedIndex = 1
+  const existingChildNames = []
+  for (let child of el.children) {
+    const childAncestors = [...ancestors]
+    const childNameBase = (() => {
+      if (child.dataset.name !== undefined) return child.dataset.name
+      else if (child.hasAttribute('id')) return child.id
+      else return `el${unnamedIndex++}`
+    })()
+    let childName = childNameBase
+    let disambiguator = 2
+    while (existingChildNames.includes(childName)) {
+      childName = `${childNameBase}${disambiguator++}`
+    }
+    childAncestors.push(childName)
+    existingChildNames.push(childName)
+    animIDs(child, childAncestors)
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-  for (let frame of ['piece-frame', 'words-frame', 'letters-frame']) {
-    const paths = document.getElementById(frame).content.querySelectorAll('path')
-    const groupIndices = {}
-    for (let path of paths) {
-      if (!path.hasAttribute('id')) {
-        let el = path
-        while (el !== null && !el.id) el = el.parentElement
-        const groupId = el.id
-        if (!groupIndices[groupId]) groupIndices[groupId] = 1
-        path.id = `${groupId}-path${groupIndices[groupId]++}`
-      }
-    }
+  for (let i = 1; i <= 4; i++) {
+    animIDs(document.getElementById(`frame-${i}`).content.querySelector('svg'))
   }
 
-  const frames = [
-    document.getElementById('piece-frame').innerHTML,
-    document.getElementById('words-frame').innerHTML,
-    document.getElementById('letters-frame').innerHTML,
-  ]
+  const frames = [1, 2, 3, 3, 4].map(n => document.getElementById(`frame-${n}`).innerHTML);
 
   const morphses = []
   for (let frame of frames) {
@@ -45,49 +61,40 @@ document.addEventListener("DOMContentLoaded", () => {
     MorphSVGPlugin.convertToPath("polygon")
 
     const morphs = {}
-    for (let p of document.querySelectorAll('#frame-container path')) {
-      if (! p.id) {
-        // console.log('oh no')
-        continue
-      }
-      morphs[p.id] = p
+    for (let p of document.querySelectorAll('#frame-container path[data-anim-id]')) {
+      morphs[p.dataset['anim-id']] = p
     }
     
     morphses.push(morphs)
   }
 
-  console.log(morphses)
-
   frameContainer.innerHTML = frames[0]
   MorphSVGPlugin.convertToPath("polygon")
 
+  let lastTime = 0
   for (let i = 1; i < frames.length; i++) {
     const morphs = morphses[i]
     for (let key in morphs) {
       tl.to(
-        `#${key}`,
-        { morphSVG: morphs[key].getAttribute("d"), duration: 1, ease: 'linear' },
-        i - 1
+        `[data-anim-id="${key}"]`,
+        { morphSVG: morphs[key].getAttribute("d"), duration: durations[i - 1], ease: 'linear' },
+        lastTime
       );
     }
 
-
-    tl.fromTo(
-      "#words",
-      {opacity: opacities[i-1].words},
-      {opacity: opacities[i].words, duration: 1},
-      i - 1
-    )
-
-    tl.fromTo(
-      "#letters",
-      {opacity: opacities[i-1].letters},
-      {opacity: opacities[i].letters, duration: 1},
-      i - 1
-    )
+    for (let layerName of ['words', 'connectors', 'letters']) {
+      tl.fromTo(
+        `#${layerName}`,
+        {opacity: opacities[i-1][layerName]},
+        {opacity: opacities[i][layerName], duration: durations[i - 1]},
+        lastTime
+      )
+    }
+    lastTime += durations[i-1]
   }
 
   gsap.set("#words", {opacity: 0})
+  gsap.set("#connectors", {opacity: 0})
   gsap.set("#letters", {opacity: 0})
 })
 
